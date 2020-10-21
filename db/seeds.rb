c645dbbe69c811785d09434c745e2b04dde79990
if6600 <60003219+if6600@users.noreply.github.com>
2020-10-22 01:32:51 +0300

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 Post.destroy_all
-puts "Destroyed everything you touch"
+
 posts = [
 {
-	name: "New Album",
-	title: "The Bad Seeds",
-	author: "Nick Cave",
-	content: "Tracklist",
-	image: File.open(Rails.root.join('public', 'images', 'pic-1.png'))
+	name: "собака",
+	title: "Шарик",
+	content: "Лиза",
+	image: File.open(Rails.root.join('public', 'images', 'picturecat.png'))
 },
 {
-	name: "Old Album",
-	title: "The Bad Seeds Old",
-	author: "Nick Cave Old Too",
-	content: "Some Old Tracklist",
-	image: open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
+	name: "кошка",
+	title: "Пушок",
+	content: "Полина",
+	image: File.open(Rails.root.join('public', 'images', 'picturedog.png'))
 }
 ]
 
