8f1bffd5ce97f55343e5c4119491f3249881367f
Avbey <azovsky777@gmail.com>
2021-04-01 02:31:41 +0300

    Added user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-			devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:image, :email, :password, :password_confirmation, :current_password) }
+			devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:image, :email, :password, :password_confirmation, :current_password) }
     end
 end
