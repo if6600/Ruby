b3470004da9a2232a53e09d48b918e0f72e4767e
Avbey <azovsky777@gmail.com>
2021-05-03 17:59:28 +0300

    Added db seeds

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web:  rails server -p ${PORT:-5000} -e $RAILS_ENV
-release: rails db:migrate
+release: rails db:migrate && rails db:seed
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,56 @@
 Post.destroy_all
-puts "Destroyed everything you touch"
-#posts = [
-#{
-#	name: "собака",
-#	title: "Шарик",
-#	content: "Лиза",
-#	image: File.open(Rails.root.join('public', 'images', 'picturecat.png'))
-#},
-#{
-#	name: "кошка",
-#	title: "Пушок",
-#	content: "Полина",
-#	image: File.open(Rails.root.join('public', 'images', 'picturedog.png'))
-#}
-#]
+Category.destroy_all
+User.destroy_all
 
-#posts.each do |post|
-#	postik = Post.create(post)
-#	puts "Some magic just create a #{ postik.name } with id #{ postik.id }!"
-#end
+admin = User.new(
+  :email                 => "admin@first.dog",
+  :password              => "123456",
+  :password_confirmation => "123456",
+  :isadmin               => true
+)
+admin.save!
+puts "Created admin with id #{ admin.id }"
+
+categories = [
+  {
+	name: "Категория 1",
+	description: "Описание категории 1"
+  },
+	{
+		name: "Категория 2",
+		description: "Описание категории 2"
+	},
+]
+
+categories.each do |category|
+	category_ = Category.create(category)
+	category_.save!
+	puts "Create category #{ category_.name } with id #{ category_.id }!"
+end
+
+posts = [
+{
+	name: "собака",
+	title: "Шарик",
+	content: "Лиза",
+  user_id: User.all.ids[0],
+  category_id: Category.all.ids[0],
+	image: File.open(Rails.root.join('public', 'images', 'picturecat.png'))
+},
+{
+	name: "кошка",
+	title: "Пушок",
+	content: "Полина",
+	user_id: User.all.ids[0],
+	category_id: Category.all.ids[1],
+	image: File.open(Rails.root.join('public', 'images', 'picturedog.png'))
+}
+]
+
+puts posts
+
+posts.each do |post|
+	post_ = Post.create(post)
+  post_.save!
+	puts "Created post #{ post_.name } with id #{ post_.id }!"
+end
