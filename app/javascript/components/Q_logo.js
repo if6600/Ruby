3a6a4d27b37c57f613be6755399ccaefc87db429
Avbey <azovsky777@gmail.com>
2021-06-15 17:45:49 +0300

    Added link to logo

--- app/javascript/components/Q_logo.js
+++ app/javascript/components/Q_logo.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types"
 import A_picture from "./A_picture"
 
 class A_logo extends React.Component {
-  render () {
-    return (
-      <A_picture src="/assets/logo.png" width={this.props.width} height={this.props.height}/>
-    );
-  }
+    render() {
+        return (
+            <a href={"/"}>
+                <A_picture src="/assets/logo.png" width={this.props.width} height={this.props.height}/>
+            </a>
+        );
+    }
 }
 
 export default A_logo
