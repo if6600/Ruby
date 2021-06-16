c8722014ebd2dc32c80d46f6a287eea1a9a7ce89
Avbey <azovsky777@gmail.com>
2021-06-16 17:10:38 +0300

    Seeds updated [1]

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,12 +71,6 @@ promos = [
   },
   {
     title: "Новость 3",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    user_id: User.all.ids[0],
-    image: File.open(Rails.root.join('public', 'images', 'promo3.jpg'))
-  },
-  {
-    title: "Новость 4",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     user_id: User.all.ids[0],
     image: File.open(Rails.root.join('public', 'images', 'promo3.jpg'))
