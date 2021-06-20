ad8a18adeec0a63a5412d1f63d5741733e419d6d
Avbey <azovsky777@gmail.com>
2021-06-20 18:38:56 +0300

    Added tags to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -95,6 +95,6 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:title, :content, :image, :category_id, :is_published)
+    params.require(:post).permit(:title, :content, :image, :category_id, :is_published, :tag_list)
   end
 end
