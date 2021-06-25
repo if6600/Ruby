46cef510acf28f327dcb1bc87231db3f14528cdb
Avbey <azovsky777@gmail.com>
2021-06-25 21:56:20 +0300

    Fix styles/misc

--- app/javascript/components/atoms/logo/Logo.js
+++ app/javascript/components/atoms/logo/Logo.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import defaultLogo from "../../../../assets/images/logo.png";
 
 const Logo = props => {
-    return <a href="/"><img src={defaultLogo} alt="Logo" {...props} /></a>
+    return <a href="https://anutavsesuper.github.io/4-module-lending/"><img src={defaultLogo} alt="Logo" {...props} /></a>
 };
 
 export default Logo;
